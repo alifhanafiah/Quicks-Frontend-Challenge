d6538e5bb16b7635ffb95537e3eabadb2cc8478f
alifhanafiah <muhalifhanafiah@gmail.com>
2023-12-19 22:41:40 +0800

    Starting to work on the animation

--- src/components/QuicksNav.tsx
+++ src/components/QuicksNav.tsx
@@ -1,10 +1,31 @@
+import { useState } from "react";
+import TaskColored from "../assets/icons/chrome_reader_mode_colored.svg";
 import Lightning from "../assets/icons/cloud-lightning.svg";
+import InboxColored from "../assets/icons/question_answer_colored.svg";
 
 const QuicksNav = () => {
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div className="fixed bottom-0 right-0 border border-red-500">
-      <div className="flex h-16 w-16 items-center rounded-full bg-primary-blue">
-        <img src={Lightning} alt="Quicks Shorcut" />
+    <div className="fixed bottom-6 right-6 flex flex-row-reverse items-center gap-5 md:gap-8">
+      <button className="h-68 w-68 relative z-50 grid place-items-center rounded-full bg-primary-blue">
+        <img src={Lightning} alt="Quicks" />
+      </button>
+
+      <div>
+        {/* <p className="mb-2 text-center text-white">Inbox</p> */}
+
+        <button className="relative grid h-60 w-60 translate-x-20 place-items-center rounded-full bg-[#f2f2f2] md:translate-x-24">
+          <img src={InboxColored} alt="Inbox" />
+        </button>
+      </div>
+
+      <div>
+        {/* <p className="mb-2 text-center text-white">Task</p> */}
+
+        <button className="relative grid h-60 w-60 translate-x-40 place-items-center rounded-full bg-[#f2f2f2] md:translate-x-48">
+          <img src={TaskColored} alt="Task" />
+        </button>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: {
-          blue: '#3498db',
+          blue: "#3498db",
           gray: {
-            400: '#4f4f4f',
-            500: '#828282',
-            200: '#e0e0e0',
+            400: "#4f4f4f",
+            500: "#828282",
+            200: "#e0e0e0",
           },
         },
+      },
+
+      width: {
+        68: "4.25rem",
+        60: "3.75rem",
+      },
+
+      height: {
+        68: "4.25rem",
+        60: "3.75rem",
+      },
+
+      fontFamily: {
+        lato: ["Lato", "sans-serif"],
       },
     },
   },
