e4fbc627249366a628381ae0ed77f8505e07f9ef
alifhanafiah <muhalifhanafiah@gmail.com>
2023-12-20 21:25:31 +0800

    Finishing the sliding animation

--- src/components/QuicksNav.tsx
+++ src/components/QuicksNav.tsx
@@ -8,22 +8,47 @@ const QuicksNav = () => {
 
   return (
     <div className="fixed bottom-6 right-6 flex flex-row-reverse items-center gap-5 md:gap-8">
-      <button className="h-68 w-68 relative z-50 grid place-items-center rounded-full bg-primary-blue">
+      <button
+        onClick={() => {
+          setShowNav((prev) => !prev);
+        }}
+        className="relative z-50 grid h-68 w-68 place-items-center rounded-full bg-primary-blue"
+      >
         <img src={Lightning} alt="Quicks" />
       </button>
 
-      <div>
-        {/* <p className="mb-2 text-center text-white">Inbox</p> */}
+      <div className="relative">
+        <p
+          className={`absolute bottom-16 left-1/2 -translate-x-1/2 text-center text-white transition-opacity ${
+            showNav ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          Inbox
+        </p>
 
-        <button className="relative grid h-60 w-60 translate-x-20 place-items-center rounded-full bg-[#f2f2f2] md:translate-x-24">
+        <button
+          className={`relative grid h-60 w-60 place-items-center rounded-full bg-[#f2f2f2] transition-transform ${
+            showNav ? "translate-x-20 md:translate-x-24" : "translate-x-0"
+          }`}
+        >
           <img src={InboxColored} alt="Inbox" />
         </button>
       </div>
 
-      <div>
-        {/* <p className="mb-2 text-center text-white">Task</p> */}
+      <div className="relative">
+        <p
+          className={`absolute bottom-16 left-1/2 -translate-x-1/2 text-center text-white transition-opacity ${
+            showNav ? "opacity-0" : "opacity-100"
+          }`}
+        >
+          Task
+        </p>
 
-        <button className="relative grid h-60 w-60 translate-x-40 place-items-center rounded-full bg-[#f2f2f2] md:translate-x-48">
+        <button
+          className={`relative grid h-60 w-60 place-items-center rounded-full bg-[#f2f2f2] transition-transform ${
+            showNav ? "translate-x-40 md:translate-x-48" : "translate-x-0"
+          }`}
+        >
           <img src={TaskColored} alt="Task" />
         </button>
       </div>
