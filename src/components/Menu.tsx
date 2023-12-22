0c66449f01f934174bfa4bb5c898a6526c757515
alifhanafiah <muhalifhanafiah@gmail.com>
2023-12-22 11:25:46 +0800

    fix: padding size

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -14,7 +14,7 @@ const Menu = ({ isInbox, isTask }: MenuProps) => {
     <div className="absolute bottom-28 right-6 h-[40rem] w-10/12 rounded-[6px] border border-[#828282] bg-white px-8 py-7 md:h-[46.0625rem] lg:w-[45.875rem]">
       {isInbox ? <SearchBar /> : isTask ? "this is task" : null}
 
-      <div className="h-full pt-6">
+      <div className="h-full pt-[1.375rem]">
         {isLoading ? (
           <Loading
             content={
